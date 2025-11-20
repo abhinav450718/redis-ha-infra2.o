755419b7d61638ba54a29fc87f0ea14794f3a8a2
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-21 01:58:31 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,26 +1,37 @@
 variable "aws_region" {
-  type    = string
+  type = string
   default = "sa-east-1"
 }
 
 variable "project_tag" {
-  type    = string
-  default = "redis-ha-demo"
-}
-
-variable "key_name" {
-  type        = string
-  default     = "redis-ha-key"
-  description = "SSH key name for EC2 instances"
+  type = string
+  default = "redis-ha"
 }
 
 variable "ami_id" {
-  type        = string
-  default     = "ami-0fb731c579cdc06e9"
+  type = string
 }
 
 variable "bastion_ssh_cidr" {
-  description = "CIDR allowed to SSH into Bastion"
-  type        = string
-  default     = "0.0.0.0/0"
+  type = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "private1_subnet_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "private2_subnet_cidr" {
+  type    = string
+  default = "10.0.3.0/24"
 }
