02139b72326bfbaf38c6282161b6c64138a69f57
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-20 17:53:58 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "redis-ha-infra-state"
     key            = "terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "sa-east-1"
     dynamodb_table = "redis-ha-infra-lock"
     encrypt        = true
   }
