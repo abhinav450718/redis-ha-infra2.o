ac722a725f4b594887e6a7aeb8f24390860118bc
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-20 17:56:06 +0530

    Create main.tf

---
+++ terraform/modules/redis-master/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "master" {
+  ami                    = var.ami_id
+  instance_type          = "t3.micro"
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.key_name
+
+  tags = {
+    Name    = "master-redis"
+    Role    = "master-redis"
+    Project = var.project_tag
+  }
+}
