a730e329037b5b14d232f77b7508981de1a318cb
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-20 19:26:46 +0530

    Update verify_redis.sh

--- ansible/verify_redis.sh
+++ ansible/verify_redis.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 KEY=$1
-INVENTORY="../ansible/inventory/aws_ec2.yml"
+INV="../ansible/inventory/aws_ec2.yml"
 
 echo "=== Fetching inventory info ==="
 
-MASTER=$(ansible-inventory -i $INVENTORY --host master | jq -r '.ansible_host')
-REPLICA=$(ansible-inventory -i $INVENTORY --host replica | jq -r '.ansible_host')
+MASTER=$(ansible-inventory -i $INV --host master | jq -r '.ansible_host')
+REPLICA=$(ansible-inventory -i $INV --host replica | jq -r '.ansible_host')
 
 echo "Master: $MASTER"
 echo "Replica: $REPLICA"
 
-echo "=== Checking Redis Master Replication Info ==="
+echo "=== Checking Redis Master ==="
 ssh -o StrictHostKeyChecking=no -i "$KEY" ec2-user@$MASTER "redis-cli INFO replication | grep role"
 
-echo "=== Checking Redis Replica Replication Info ==="
+echo "=== Checking Redis Replica ==="
 ssh -o StrictHostKeyChecking=no -i "$KEY" ec2-user@$REPLICA "redis-cli INFO replication | grep -E 'role|master_host|master_link_status'"
