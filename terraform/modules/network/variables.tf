f0048eecc3118fccdd35dc5768e34eeb65c3cb36
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-21 01:09:56 +0530

    Update variables.tf

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,15 +1,34 @@
 variable "vpc_cidr" {
-  type = string
+  type        = string
+  description = "CIDR for the VPC"
 }
 
 variable "public_subnet_cidr" {
-  type = string
+  type        = string
+  description = "CIDR for public subnet"
 }
 
-variable "private_subnet1_cidr" {
-  type = string
+variable "private1_subnet_cidr" {
+  type        = string
+  description = "CIDR for private subnet 1 (master)"
 }
 
-variable "private_subnet2_cidr" {
-  type = string
+variable "private2_subnet_cidr" {
+  type        = string
+  description = "CIDR for private subnet 2 (replica)"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region, e.g., sa-east-1"
+}
+
+variable "project_tag" {
+  type        = string
+  description = "Tag for project name"
+}
+
+variable "bastion_ssh_cidr" {
+  type        = string
+  description = "Your public IP/CIDR allowed to SSH into bastion"
 }
