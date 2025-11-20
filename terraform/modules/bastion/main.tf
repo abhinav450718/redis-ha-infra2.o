1dd3e11ef53002e835d44dc16686ca907d260df2
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-20 17:55:13 +0530

    Create main.tf

---
+++ terraform/modules/bastion/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "bastion" {
+  ami                    = var.ami_id
+  instance_type          = "t3.micro"
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.key_name
+
+  tags = {
+    Name    = "bastion-host"
+    Role    = "bastion"
+    Project = var.project_tag
+  }
+}
