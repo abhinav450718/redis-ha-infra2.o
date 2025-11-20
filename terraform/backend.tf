a536da74c5cd113e53246635ada3254737059792
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-21 01:25:52 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "redis-ha-infra-state"
-    key            = "terraform.tfstate"
-    region         = "sa-east-1"
-    dynamodb_table = "redis-ha-infra-lock"
-    encrypt        = true
-  }
-}
