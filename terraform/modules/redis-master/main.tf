0363b6c832d65f973632acc7c93f6669b04b7944
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-21 12:43:57 +0530

    Update main.tf

--- terraform/modules/redis-master/main.tf
+++ terraform/modules/redis-master/main.tf
@@ -1,15 +1,12 @@
-resource "aws_instance" "master" {
+resource "aws_instance" "redis_master" {
   ami                    = var.ami_id
   instance_type          = "t3.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
-
-  # IMPORTANT — always use the key created by Terraform
-  key_name = var.key_name
+  key_name               = var.key_name
 
   tags = {
     Name    = "redis-master"
-    Role    = "master"
     Project = var.project_tag
   }
 }
