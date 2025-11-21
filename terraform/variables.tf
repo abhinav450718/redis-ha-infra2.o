fb2e0d0df20b3ba1d7fce55b22a5335aea2e317f
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-21 12:40:57 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,10 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
+variable "aws_region" { type = string }
 
-variable "project_tag" {
-  type    = string
-  default = "redis-ha"
-}
+variable "vpc_cidr" { type = string }
+variable "public_subnet_cidr" { type = string }
+variable "private1_subnet_cidr" { type = string }
+variable "private2_subnet_cidr" { type = string }
 
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr" {
-  type    = string
-  default = "10.0.1.0/24"
-}
-
-variable "private1_subnet_cidr" {
-  type    = string
-  default = "10.0.2.0/24"
-}
-
-variable "private2_subnet_cidr" {
-  type    = string
-  default = "10.0.3.0/24"
-}
-
-variable "bastion_ssh_cidr" {
-  type    = string
-  default = "0.0.0.0/0"
-}
-
-variable "ami_id" {
-  type    = string
-  default = "ami-04a81a99f5ec58529"
-}
+variable "project_tag" { type = string }
+variable "bastion_ssh_cidr" { type = string }
+variable "ami_id" { type = string }
