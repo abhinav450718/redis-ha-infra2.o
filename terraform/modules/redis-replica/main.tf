cd5245a1f6c7cd793ba1da2ea633c3826f7849ea
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-21 12:44:48 +0530

    Update main.tf

--- terraform/modules/redis-replica/main.tf
+++ terraform/modules/redis-replica/main.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "redis_replica" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
+  ami                    = var.ami_id
+  instance_type          = "t3.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
 
   tags = {
-    Name = "redis-replica"
+    Name    = "redis-replica"
+    Project = var.project_tag
   }
 }
