175842d24217236f62338d3a647985e9350e5df8
abhinav450718 <abhinavsikarwar011@gmail.com>
2025-11-21 12:56:54 +0530

    Update main.tf

--- terraform/modules/redis-master/main.tf
+++ terraform/modules/redis-master/main.tf
@@ -1,12 +1,21 @@
-resource "aws_instance" "redis_master" {
-  ami                    = var.ami_id
-  instance_type          = "t3.micro"
-  subnet_id              = var.subnet_id
+variable "subnet_id" {}
+variable "security_group_id" {}
+variable "key_name" {}
+variable "ami_id" {}
+variable "project_tag" {}
+
+resource "aws_instance" "master" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+  subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
-  key_name               = var.key_name
+  key_name      = var.key_name
 
   tags = {
-    Name    = "redis-master"
-    Project = var.project_tag
+    Name = "${var.project_tag}-redis-master"
   }
+}
+
+output "private_ip" {
+  value = aws_instance.master.private_ip
 }
